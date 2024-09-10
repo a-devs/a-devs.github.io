710bde94cb1ee447dea93752314ae70b0936086b
a-devs <67658363+a-devs@users.noreply.github.com>
2024-09-10 14:09:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ document.getElementById("generator").onclick = function() {
   var episode = getRandomInt(1, episodesPerSeason[season]);
 
   document.title = `S${season}E${episode}`;
-  frame.src = `https://binged.to/watch/tv/387?season=${season}&ep=${episode}`;
+  frame.src = `https://vidsrc.me/embed/tv?imdb=tt0206512&season=${season}&episode=${episode}`;
 
   frame.style.opacity = 1;
 }
